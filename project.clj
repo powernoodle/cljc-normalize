78c9d53bc8c086f9ac7ebf240bf7341e7d42daa7
Brendon Walsh <brendonwalsh@niamu.com>
2015-10-13 14:43:23 -0400

    License change to match normalize.css project

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject normalize "0.1.0-SNAPSHOT"
+(defproject com.powernoodle/normalize "0.1.0"
   :description "Clojure library that supplies normalize.css via Garden"
-  :url "http://github.com/powernoodle/normalize"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "http://github.com/powernoodle/cljc-normalize"
+  :license {:name "Copyright"
+            :url "https://github.com/necolas/normalize.css/blob/master/LICENSE.md"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.48"]
                  [garden "1.3.0-SNAPSHOT"]]
