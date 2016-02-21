97eb8a1402de9d417520939e0c1d5e2bb3069f48
Brendon Walsh <brendonwalsh@niamu.com>
2016-02-20 21:35:52 -0500

    Version bump dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject com.powernoodle/normalize "0.1.1"
+(defproject com.powernoodle/normalize "0.1.2"
   :description "Clojure library that supplies normalize.css via Garden"
   :url "http://github.com/powernoodle/cljc-normalize"
   :license {:name "Copyright"
             :url "https://github.com/necolas/normalize.css/blob/master/LICENSE.md"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.48"]
-                 [garden "1.3.0-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.7.228"]
+                 [garden "1.3.1"]]
   :main normalize.core)
